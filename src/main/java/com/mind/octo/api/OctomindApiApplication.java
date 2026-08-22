85bc921ad467dbe04ca21b6aa6d9ebac116948ab
lunaridelmar <lunaridelmar@gmail.com>
2026-08-22 19:40:42 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/mind/octo/api/OctomindApiApplication.java
@@ -0,0 +1,13 @@
+package com.mind.octo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctomindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctomindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mind/octo/api/OctomindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mind.octo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctomindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
